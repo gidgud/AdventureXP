8bf35e73c9e56c1d1bc10fdf84dbdbe9caf426ac
gidgud <jonathanbirkstrom@gmail.com>
2026-03-03 14:47:07 +0100

    created activity repository

---
+++ src/main/java/com/xpgruppe/AdventureXP/repository/ActivityRepository.java
@@ -0,0 +1,10 @@
+package com.xpgruppe.AdventureXP.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.xpgruppe.AdventureXP.model.Activity;
+
+public interface ActivityRepository extends JpaRepository<Activity, Long> {
+
+
+}
