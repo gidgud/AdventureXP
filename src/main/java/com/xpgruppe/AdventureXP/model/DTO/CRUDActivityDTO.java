886b77c8ab7c2faa90adc2943beb9fa4fabf60e2
Yakoob1884 <vivja.aty@gmail.com>
2026-03-09 10:27:15 +0100

    Created Crud activity DTO

---
+++ src/main/java/com/xpgruppe/AdventureXP/model/DTO/CRUDActivityDTO.java
@@ -0,0 +1,16 @@
+package com.xpgruppe.AdventureXP.model.DTO;
+
+import com.xpgruppe.AdventureXP.model.Equipment;
+
+import java.util.ArrayList;
+
+public class CRUDActivityDTO {
+
+    private Long id;
+    private String activityName;
+    private String activityDetail;
+    private String activityRequimentDetail;
+    private ArrayList<Equipment> equipments;
+
+
+}
